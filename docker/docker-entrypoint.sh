5a6ce9eb2903b48ec7d848a0e1669a7cbaa71532
ANLO.PO <anlo.po@gmail.com>
2024-06-17 16:18:30 +0200

    Run server with ssl in docker with local CA

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,5 +5,10 @@ if [ ! -e "config/config.yaml" ]; then
     cp -r "default/config.yaml" "config/config.yaml"
 fi
 
+# Add local CA authority to node
+export NODE_EXTRA_CA_CERTS=/home/node/app/certs/myCA.pem
+
 # Start the server
-exec node server.js --listen
+#exec node server.js --listen
+# Start the server with SSL
+exec node server.js --listen --ssl
